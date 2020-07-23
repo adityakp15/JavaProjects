338519b7bfe04deda041fbf1746727401fa6366f
adityakp15 <aditponnu@gmail.com>
2020-07-24 01:45:26 +0530

    finished pokemon api integration

--- Python Project/pokedex.py
+++ Python Project/pokedex.py
@@ -1,6 +1,41 @@
 import requests
 
-def get_data:
-    base_url = "https://pokeapi.co/api/v2/pokemon"
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_data(choice):
+    #base_url = "https://pokeapi.co/api/v2/pokemon"
+    base = base_url + choice
     params = {}
+    params["offest"] = 0
+    params["limit"] = 964
+    resp = requests.get(base,params)
+    #print(resp.json())
+    #print(resp.url)
+    print_pokemon(resp.json(),params["limit"])
+
+def print_pokemon(data,limit):
+    for i in range(0,limit):
+        print("Pokemon {} is {}".format(i+1,data["results"][i]["name"]))
+
+def get_choice():
+    print("What would you like to know ? ")
+    choices = {
+        1 : "Pokemon",
+        2 : "Evolution",
+        3 : "Berries",
+        4 : "Location",
+        5 : "Moves"
+    }
+
+    for choice in choices:
+        print(choice,": ",choices[choice])
+
+    choice = int(input("Enter your choice : "))
+    if(choice == 1):
+        get_pokemon_data("pokemon")
+
+def main():
     
+    get_choice()
+
+main()
