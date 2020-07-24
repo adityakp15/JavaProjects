5c0b943acc2021b4cf27ac2bf8ab3da099168079
adityakp15 <aditponnu@gmail.com>
2020-07-24 15:56:21 +0530

    finished pokedex project, evolutions not possible

--- Python Project/pokedex.py
+++ Python Project/pokedex.py
@@ -42,6 +42,7 @@ def get_evolution_data(choice):
         print(d["chain"]["species"]["name"])
         print(d["chain"]["evolves_to"][0]["species"]["name"])
         print(d["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["name"])
+        print("Sorry, but thats it :( ")
         break
         
 
