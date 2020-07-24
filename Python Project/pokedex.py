dfeb61f741d86cc9456c7ddebcc54a6237efd302
adityakp15 <aditponnu@gmail.com>
2020-07-24 15:41:13 +0530

    finished pokedex project, evolution not working

--- Python Project/pokedex.py
+++ Python Project/pokedex.py
@@ -1,9 +1,10 @@
 import requests
+import json
 
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_data(choice):
-    #base_url = "https://pokeapi.co/api/v2/pokemon"
+
     base = base_url + choice
     params = {}
     params["offest"] = 0
@@ -11,20 +12,55 @@ def get_pokemon_data(choice):
     resp = requests.get(base,params)
     #print(resp.json())
     #print(resp.url)
-    print_pokemon(resp.json(),params["limit"])
+    print_details(resp.json(),params["limit"],choice)
 
-def print_pokemon(data,limit):
-    for i in range(0,limit):
-        print("Pokemon {} is {}".format(i+1,data["results"][i]["name"]))
+def get_berry_data(choice):
+
+    base =  base_url + choice
+    params = {}
+    params["offest"] = 0
+    params["limit"] = 64
+    resp = requests.get(base,params)
+    print(resp.url)
+    #print(resp.json())
+    print_details(resp.json(),params["limit"],choice)
+
+def get_evolution_data(choice):
+
+    base =  base_url + choice
+    params = {}
+    params["offest"] = 0
+    params["limit"] = 419
+    resp = requests.get(base,params)
+    #print(resp.url)
+    #print(resp.json())
+    data = resp.json()
+    for i in range(0,params["limit"]):
+        #print(data["results"][i]["url"])
+        ev_data = requests.get(data["results"][i]["url"])
+        d = ev_data.json()
+        print(d["chain"]["species"]["name"])
+        print(d["chain"]["evolves_to"][0]["species"]["name"])
+        print(d["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["name"])
+        
+
+def print_details(data,limit,choice):
+
+    if(choice == "pokemon"):
+        for i in range(0,limit):
+            print("Pokemon #{} is {}".format(i+1,data["results"][i]["name"].upper()))
+    elif(choice == "berry"):
+        for i in range(0,limit):
+            print("The available berries are {} berry".format(data["results"][i]["name"].upper()))
 
 def get_choice():
+
     print("What would you like to know ? ")
     choices = {
         1 : "Pokemon",
         2 : "Evolution",
         3 : "Berries",
-        4 : "Location",
-        5 : "Moves"
+        4 : "Quit"
     }
 
     for choice in choices:
@@ -33,6 +69,15 @@ def get_choice():
     choice = int(input("Enter your choice : "))
     if(choice == 1):
         get_pokemon_data("pokemon")
+    elif(choice == 2):
+        get_evolution_data("evolution-chain")
+    elif(choice == 3):
+        get_berry_data("berry")
+    elif(choice == 4):
+        quit()
+    
+    
+    
 
 def main():
     
